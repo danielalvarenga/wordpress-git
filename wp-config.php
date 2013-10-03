e70a41d67684394f9b61543bd7dfcea697b83572
Daniel Alvarenga <danielalvarengalima@gmail.com>
2013-10-03 14:24:19 -0300

    Atualizaçãodas configurações em wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -14,15 +14,22 @@
  * @package WordPress
  */
 
+// ** Adicionado para trabalhar com reporitório git ** //
+define('WP_CONTENT_DIR', $_SERVER['DOCUMENT_ROOT'] . '/wp-content');
+define('WP_CONTENT_URL', 'http://' . $_SERVER['SERVER_NAME'] . '/wp-content');
+
+// ** Especificar o nome do Thema Wordpress, caso não esteja usando o "twentyeleven" ** //
+define('WP_DEFAULT_THEME', 'academica_pro');
+
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+define('DB_NAME', 'wpbd_fecr');
 
 /** MySQL database username */
-define('DB_USER', 'username_here');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
@@ -78,7 +85,7 @@ define('WPLANG', '');
  * It is strongly recommended that plugin and theme developers use WP_DEBUG
  * in their development environments.
  */
-define('WP_DEBUG', false);
+define('WP_DEBUG', true);
 
 /* That's all, stop editing! Happy blogging. */
 
